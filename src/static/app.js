715f1fefceb9dfa0119a28cc339c843cf53246e9
Wesley Schwartz <93950028+wesleyschwartz@users.noreply.github.com>
2025-06-13 21:03:18 +0000

    Adiciona lista de participantes nas atividades

--- src/static/app.js
+++ src/static/app.js
@@ -25,6 +25,19 @@ document.addEventListener("DOMContentLoaded", () => {
           <p>${details.description}</p>
           <p><strong>Schedule:</strong> ${details.schedule}</p>
           <p><strong>Availability:</strong> ${spotsLeft} spots left</p>
+          <div>
+            <strong>Participants:</strong>
+            ${
+              details.participants.length > 0
+                ? `<ul>${details.participants
+                    .map(
+                      (participant) =>
+                        `<li>${participant}</li>`
+                    )
+                    .join("")}</ul>`
+                : "<em>No participants yet.</em>"
+            }
+          </div>
         `;
 
         activitiesList.appendChild(activityCard);
